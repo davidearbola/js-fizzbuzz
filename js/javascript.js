268873034b40288844e25b1ad4d8931d773706e9
davidearbola <davidearbola@gmail.com>
2024-04-19 14:35:40 +0200

    bonus 1 bonus 2

--- js/javascript.js
+++ js/javascript.js
@@ -34,14 +34,85 @@ let n = 1;
 
 
 // 4) i numeri che sono sia multipli di tre che di cinque stampo fizzbuzz (operatore modulo: % formula: n % 3 == 0 && n % 5 == 0)
+// for (n; n <= 100; n++){
+//     if (n % 3 == 0 && n % 5 == 0) {
+//         console.log("fizzbuzz");
+//     } else if (n % 3 == 0) {
+//         console.log("fizz");
+//     } else if (n % 5 == 0) {
+//         console.log("buzz");
+//     }  else {
+//         console.log(n);
+//     }
+// }
+
+
+
+// BONUS 1:
+// Crea un container nel DOM , aggiungendo (attraverso la funzione append()) un elemento html con il numero o la stringa corretta da mostrare.
+
+// for (n; n <= 100; n++){
+//     if (n % 3 == 0 && n % 5 == 0) {
+//         let n = "fizzbuzz"
+//         let printN = document.getElementById("fizzbuzz-container");
+//         printN.innerHTML = printN.innerHTML + `
+//         <p>${n}</p>
+//         `;
+//     } else if (n % 3 == 0) {
+//         let n = "fizz"
+//         let printN = document.getElementById("fizzbuzz-container");
+//         printN.innerHTML = printN.innerHTML + `
+//         <p>${n}</p>
+//         `;
+//     } else if (n % 5 == 0) {
+//         let n = "buzz"
+//         let printN = document.getElementById("fizzbuzz-container");
+//         printN.innerHTML = printN.innerHTML + `
+//         <p>${n}</p>
+//         `;
+//     }  else {
+//         let printN = document.getElementById("fizzbuzz-container");
+//         printN.innerHTML = printN.innerHTML + `
+//         <p>${n}</p>
+//         `;
+//     }
+// }
+
+// BONUS 2:
+// Applica stili differenti agli elementi aggiunti al DOM nel BONUS 1, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz.
+
 for (n; n <= 100; n++){
     if (n % 3 == 0 && n % 5 == 0) {
-        console.log("fizzbuzz")
+        let n = "FizzBuzz"
+        let printN = document.getElementById("fizzbuzz-container");
+        printN.innerHTML = printN.innerHTML + `
+        <div class="box box-fizz-buzz">
+            <span class="line-h">${n}</span>
+        </div>
+        `;
     } else if (n % 3 == 0) {
-        console.log("fizz");
+        let n = "Fizz"
+        let printN = document.getElementById("fizzbuzz-container");
+        printN.innerHTML = printN.innerHTML + `
+        <div class="box box-fizz">
+            <span class="line-h">${n}</span>
+        </div>
+        `;
     } else if (n % 5 == 0) {
-        console.log("buzz")
+        let n = "buzz"
+        let printN = document.getElementById("fizzbuzz-container");
+        printN.innerHTML = printN.innerHTML + `
+        <div class="box box-buzz">
+            <span class="line-h">${n}</span>
+        </div>
+        `;
     }  else {
-        console.log(n);
+        let printN = document.getElementById("fizzbuzz-container");
+        printN.innerHTML = printN.innerHTML + `
+        <div class="box box-number">
+            <span class="line-h">${n}</span>
+        </div>
+        `;
     }
 }
+
