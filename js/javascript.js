6205b37567f04fd9dbf479c23908ef043c985b70
davidearbola <davidearbola@gmail.com>
2024-04-22 10:14:04 +0200

    minor fix

--- js/javascript.js
+++ js/javascript.js
@@ -2,7 +2,7 @@
 // Scrivi un programma che stampi in console i numeri da 1 a 100, ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”. Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 
 // 1) stampo i numeri da 1 a 100
-let n = 1;
+// let n = 1;
 // for (n; n <= 100; n++){
 //     console.log("nuovo numero:"+n)
 // }
@@ -10,6 +10,7 @@ let n = 1;
 
 
 // 2) i numeri che sono multipli di tre stampo fizz (operatore modulo: % = formula: n % 3 == 0(vuol dire se n è multiplo di tre il risultato dell'operazione sarà 0))
+// let n = 1;
 // for (n; n <= 100; n++){
 //     if (n % 3 == 0){
 //         console.log("fizz");
@@ -21,6 +22,7 @@ let n = 1;
 
 
 // 3) i numeri che sono multipli di cinque stampo buzz (operatore modulo: % formula: n % 5 == 0)
+// let n = 1;
 // for (n; n <= 100; n++){
 //     if (n % 3 == 0){
 //         console.log("fizz");
@@ -34,6 +36,7 @@ let n = 1;
 
 
 // 4) i numeri che sono sia multipli di tre che di cinque stampo fizzbuzz (operatore modulo: % formula: n % 3 == 0 && n % 5 == 0)
+// let n = 1;
 // for (n; n <= 100; n++){
 //     if (n % 3 == 0 && n % 5 == 0) {
 //         console.log("fizzbuzz");
@@ -50,7 +53,7 @@ let n = 1;
 
 // BONUS 1:
 // Crea un container nel DOM , aggiungendo (attraverso la funzione append()) un elemento html con il numero o la stringa corretta da mostrare.
-
+// let n = 1;
 // for (n; n <= 100; n++){
 //     if (n % 3 == 0 && n % 5 == 0) {
 //         let n = "fizzbuzz"
@@ -80,7 +83,7 @@ let n = 1;
 
 // BONUS 2:
 // Applica stili differenti agli elementi aggiunti al DOM nel BONUS 1, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz.
-
+let n = 1;
 for (n; n <= 100; n++){
     if (n % 3 == 0 && n % 5 == 0) {
         let n = "FizzBuzz"
